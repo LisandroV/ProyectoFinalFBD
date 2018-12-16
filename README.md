890f3479b712cb8001b303da17ec5d29162862e4
LisandroV <vazquezlisandro673c@gmail.com>
2018-12-16 04:36:25 -0600

    flask con templates

--- README.md
+++ README.md
@@ -8,7 +8,50 @@ Proyecto final: **Asociación de taxistas**
 
 ### Integrantes
 
-* Manu | mmonrealgamboa@ciencias.unam.mx
-* Paova | Correo del alumno 2
-* Alma Rosa Páes Alcalá | alma_rpa98@ciencias.unam.mx
+* Monreal Gamboa Francisco Manuel | mmonrealgamboa@ciencias.unam.mx
+* Páes Alcalá Alma Rosa | alma_rpa98@ciencias.unam.mx
 * Vázquez Aguilar Lisandro | lisandro_xp@ciencias.unam.mx
+* Vázquez Rizo Paola | paovats@ciencias.unam.mx
+
+<hr>
+
+## Instalación del proyecto
+Para la instalación correcta se requiere tener el gestor de paquetes **pip** de python
+
+1. Se tiene que instalar _Virtual Environment:_
+```sh
+pip install virtualenv
+```
+
+2. Se tiene que crear el virtualenv desde la carpeta **src**:
+```sh
+virtualenv taxis-env;            //se crea el ambiente virtual
+source taxis-env/bin/activate;   //se activa el ambiente virtual
+```
+
+3. Se tienen que instalar los siguientes paquetes flask y psycopg2:
+```sh
+pip install flask psycopg2
+```
+
+4. En la carpeta **src** se crea el archivo **config.py** con el contenido:
+```python
+    db = {
+        'host': 'localhost',
+        'port': 5432,
+        'user': 'TU_USUARIO',
+        'password': 'TU_CONTRASEÑA',
+        'name': 'asoc_taxis',
+    }
+```
+**Nota:** es muy importante hacer todo desde la carpeta **src**
+
+<hr>
+
+## Ejecución del proyecto
+
+desde la carpeta **src** levantamos el servidor:
+```sh
+source taxis-env/bin/activate   //se activa el ambiente virtual
+python app.py                   //se levanta el servidor
+```
--- src/app.py
+++ src/app.py
@@ -1,17 +1,23 @@
 from flask import Flask, render_template
 import psycopg2
+import config
 
-con = psycopg2.connect(host='127.0.0.1', port=5432, user='postgres',
-                          password='funkfunk91', dbname='asoc_taxis')
+con = psycopg2.connect(
+    host=config.db['host'],
+    port=config.db['port'],
+    user=config.db['user'],
+    password=config.db['password'],
+    dbname=config.db['name']);
 cur = con.cursor()
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def main():
     cur.execute("SELECT * FROM test")
     items = cur.fetchall()
     print items;
     context = {
+        'server': 'localhost:5000',
         'message': 'hola',
         'items': [
             {'name': 'Lisandro', 'age': 19},
@@ -22,6 +28,55 @@ def main():
 
     con.commit();
     return render_template('index.html', **context)
+
+
+@app.route("/registro/auto", methods=['GET'])
+def registra_auto():
+    return render_template('regAuto.html')
+
+@app.route("/registro/conductor", methods=['GET'])
+def registra_conductor():
+    return render_template('regConductor.html')
+
+@app.route("/registro/usuario", methods=['GET'])
+def registra_usuario():
+    return render_template('regUsuario.html')
+
+@app.route("/registro/usuario/alumno", methods=['GET'])
+def registra_alumno():
+    context = {
+        'type_user':{
+            'name': 'Alumno',
+            'distinct': 'Facultad',
+            'text': 'Nombre de la Facultad',
+            'image': 'estudiante.png',
+        },
+    }
+    return render_template('regAllUsers.html', **context)
+
+@app.route("/registro/usuario/academico", methods=['GET'])
+def registra_academico():
+    context = {
+        'type_user':{
+            'name': 'Academico',
+            'distinct': 'Instituto',
+            'text': 'Nombre del Instituto',
+            'image': 'aula.png',
+        },
+    }
+    return render_template('regAllUsers.html', **context)
+
+@app.route("/registro/usuario/trabajador", methods=['GET'])
+def registra_trabajador():
+    context = {
+        'type_user':{
+            'name': 'Trabajador',
+            'distinct': 'Unidad',
+            'text': 'Nombre de la Unidad',
+            'image': 'redes.png',
+        },
+    }
+    return render_template('regAllUsers.html', **context)
 
 if __name__ == '__main__':
     app.run()
